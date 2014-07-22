337c4de1514e16a89abe542f8b31140ca781a2db
Kassandra Perch <kassandra@kassandraperch.com>
2014-07-21 22:57:02 -0500

    Bestiary now populates off of platforms.js

--- js/bestiary.js
+++ js/bestiary.js
@@ -1,6 +1,33 @@
 $(function(){
-  console.log(window.platforms);
+  var platformTypes = ['Wrapped', 'Native', 'Computer', 'Hybrid'],
+      entryTemplate = Handlebars.compile($('#entry-template').html()),
+      platformTemplate = Handlebars.compile($('#category-template').html());
 
-  var wrappedPlatforms = _.where(window.platforms, {'category': 'Wrapped'});
-  console.log(wrappedPlatforms);
+  platformTypes.forEach(function(platformType){
+    $('#entries').append(platformTemplate({
+      Type: platformType
+    }));
+
+    var platformsForThisType = _.where(window.platforms, {
+      category: platformType
+    });
+
+    platformsForThisType.forEach(function(platform){
+      platform.pros = [];
+      platform.cons = [];
+
+      _.forEach(platform.attributes, function(attributeValue, attribute){
+        if(attributeValue){
+          platform.pros.push(window.nodebot_attributes[attribute].pros)
+        } else {
+          platform.cons.push(window.nodebot_attributes[attribute].cons)
+        }
+      });
+
+      platform.pros = _.flatten(platform.pros);
+      platform.cons = _.flatten(platform.cons);
+
+      $('#' + platformType + '-nodebots-list').append(entryTemplate(platform));
+    });
+  })
 });
